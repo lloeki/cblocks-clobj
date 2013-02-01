cbe43f63fda95fe0d914749e48b97bb30dc7ec8b
Loic Nageleisen <loic.nageleisen@gmail.com>
2013-02-01 10:28:40 +0100

    encapsulate print, method-style

--- main.c
+++ main.c
@@ -17,13 +17,17 @@ void Post_init(Post *self) {
     *self->body = '\0';
 }
 
+void Post_printf(Post *self) {
+    printf("%s\n\n%s\n", self->author, self->body);
+}
+
 void Post_dealloc(Post *self) {
     free(self);
 }
 
 int main(void) {
     Post *post = Post_alloc();
-    printf("%s\n\n%s\n", post->author, post->body);
+    Post_printf(post);
     Post_dealloc(post);
     return 0;
 }
