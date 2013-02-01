576ed67cebb4bd87b8a8108e536b6dd4b46b2969
Loic Nageleisen <loic.nageleisen@gmail.com>
2013-02-01 21:57:55 +0100

    fix date

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2000 Loic Nageleisen <loic.nageleisen@gmail.com>
+ * Copyright © 2012 Loic Nageleisen <loic.nageleisen@gmail.com>
  *
  * This work is free. You can redistribute it and/or modify it under the terms
  * of the Do What The Fuck You Want To Public License, Version 2, as published
